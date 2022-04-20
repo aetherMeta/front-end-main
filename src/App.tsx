39e0d928afa64c357a20d16bb9e9bdbf42b11b92
wqzdev <weiqiangzhangdev@gmail.com>
2022-04-20 15:53:24 -0400

    feature: package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Router, Redirect, Route, Switch } from "react-router-dom";
 import { ResetCSS } from "@aether/uikit";
+// import ResetCSS from "./utils/ResetCSS";
+import GlobalStyle from "./style/Global";
 import ConnectWalletButton from "components/ConnectWalletButton";
 import history from "./routerHistory";
 import "./App.css";
