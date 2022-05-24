a5b4983d8fe7bd46768c914314347b0ad2a3b610
Cene <ceneodendaal1@gmail.com>
2022-05-24 09:01:02 -0400

    document info fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 import React from 'react'
 
-export default function Document() {
-  // const [_document, setDocument] = React.useState(null)
+export default class document extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-  // React.useEffect(() => {
-  //     setDocument(document)
-  // }, [])
+  render(): React.ReactElement {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>Aether Meta</title>
+          <meta name="description" content="Bringing luxury shopping to new heights" />
+          <link rel="icon" href="/static/images/favicon.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          {/* make TileColor the main purple from design */}
+          <meta name="theme-color" content="#000000" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
 
-  return (
-    <Html>
-      <Head>
-            <meta charSet="utf-8" />
-            <link rel="icon" href="%PUBLIC_URL%/images/favicon.svg" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="theme-color" content="#000000" />
-            <meta
-            name="description"
-            content="Bringing luxury shopping to new heights"
-            />
-            <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    )
+  } 
 } 
