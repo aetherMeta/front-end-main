ea6ad7cf2a093e0b129715b5f60004d51f5bc2f7
Cene <ceneodendaal1@gmail.com>
2022-05-24 09:07:47 -0400

    soon page added

---
+++ pages/collections/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../../components/Layout/Page";
+import Soon from "../../components/Soon";
+
+const Collections: React.FC = () => {
+  return (
+    <Page>
+        <Soon/>
+    </Page>
+  );
+};
+
+export default Collections;
