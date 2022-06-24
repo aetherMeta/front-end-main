d5c04d092ba66b6267b6f5ef6721afff9a3867e7
Jack-Byte-Java <anomonus456@gmail.com>
2022-06-24 10:35:22 -0400

    Finished most of front end, may need minor tweeks

--- src/views/Contact/social/index.tsx
+++ src/views/Contact/social/index.tsx
@@ -1,30 +1,63 @@
 import React from "react";
 import styled from "styled-components";
-import { Flex, Text } from "@aethermeta/uikit";
+import { Flex, Text, MailIcon, DiscordIcon, Button } from "@aethermeta/uikit";
 
 const Container = styled.div`
-    padding 50px 56px;  
+    padding 50px 0px 50px 56px;  
 `
 
 const FlexContainer = styled(Flex) `
     justify-content: space-between;
 `
 
+const FlexContainer2 = styled(Flex) `
+    flex-direction: column;
+    justify-content: space-around;
+    
+`
+
 const StyledMoon = styled.div`
     background: url(/images/contactMoon.svg);
     background-repeat: no-repeat;
-    height: 40em;
-    width: 40em;
+    height: 40rem;
+    width: 40rem;
+`
 
+const ContactButton = styled(Button)`
+    max-width: 50%;
+`
+
+const JoinUs = styled(Text)`
+    max-width: 29rem;
 `
 
 const Social: React.FC = () => {
     return (
         <Container>
             <FlexContainer>
-                <Text variant="h2">
-                        GET IN TOUCH
-                </Text>
+                <FlexContainer2>
+                    <JoinUs variant="h2Bold">
+                            GET IN TOUCH
+                    </JoinUs>
+                    <JoinUs variant="body">
+                        We&rsquo;re on Discord. Join the conversation.
+                    </JoinUs>
+                    <ContactButton variant="primary" startIcon={<DiscordIcon />}>
+                        AetherMeta Discord
+                    </ContactButton>
+                    <JoinUs variant="h3Bold">
+                        Join us
+                    </JoinUs>
+                    <JoinUs variant="body">
+                        For a more private conversation on forming a brand partnership or career enquiry, email us.
+                    </JoinUs>
+                    <ContactButton variant="primary" startIcon={<MailIcon />}>
+                        Partnership Enquires
+                    </ContactButton>
+                    <ContactButton variant="primary" startIcon={<MailIcon />}>
+                        Join the AetherMeta team
+                    </ContactButton>
+                </FlexContainer2>
                 <StyledMoon />
             </FlexContainer>
         </Container>
