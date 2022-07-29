97bdb42f822a34bfdb594c44fcf85bc2a015e178
Jack-Byte-Java <anomonus456@gmail.com>
2022-07-28 20:45:20 -0400

    pagination wip

--- src/views/Collections/index.tsx
+++ src/views/Collections/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Page from "components/Layout/Page";
 import { useMatchBreakpoints } from "@aethermeta/uikit";
 import Items from "./items";
+import testItems from "./testItems";
 
 const Collections: React.FC = () => {
     const { isTablet, isMobile } = useMatchBreakpoints();
     return (
       <Page>
         {/* {isTablet || isMobile ? <PartnersTablet /> : <Partners />}  */}
-        <Items />
+        <Items {testItems}/>
       </Page>   
       );
     };
---
+++ src/views/Collections/testItems.ts
@@ -0,0 +1,245 @@
+const testItems = [
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Latest Drop",
+      highestBid: "2.5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Newest Drop",
+      highestBid: "5",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Coolest Drop",
+      highestBid: "7",
+      image: "url(/images/shopImage.svg)",
+    },
+    {
+      name: "The Oldest Drop",
+      highestBid: "1.5",
+      image: "url(/images/shopImage.svg)",
+    },
+  ];
+  
+  export default testItems;
+  
