c58a7538889c04aeeb9e67b378575e104ba7ab61
Jack-Byte-Java <anomonus456@gmail.com>
2022-08-25 10:26:55 -0400

    added form fix

--- src/components/PartnershipModal/index.tsx
+++ src/components/PartnershipModal/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useCallback } from "react";
-import styled from "styled-components";
 import {
   Button,
   Modal,
@@ -9,19 +8,10 @@ import {
   Text,
   Spinner,
   IconButton,
-  Link,
-  Checkbox,
-  Flex,
 } from "@aethermeta/uikit";
 import useToast from "hooks/useToast";
-
-const InlineLink = styled(Link)`
-  display: inline-block;
-`;
-
-const InlineText = styled(Text)`
-  display: inline-block;
-`;
+import { useUser } from "store/user/hooks";
+import styled from "styled-components";
 
 export interface Values {
   contactName: string;
@@ -46,42 +36,50 @@ export interface Errors {
   companyWebsite: boolean;
 }
 
+
+const StyledModal = styled(Modal)`
+  min-width: 500px;
+`;
+
 interface PartnershipModalProps {
   onSubmit: (e, values: Values) => void;
   onDismiss?: () => void;
+  fromMetaverse?: boolean;
 }
 
 const initialValues = {
-  contactName: "",
-  contactEmail: "",
-  jobTitle: "",
-  companyName: "",
-  companyDescription: "",
-  companyAddress: "",
-  companyWebsite: "",
-  companyTwitter: "",
-  companyInstagram: "",
-  additionalMessage: "",
+  name: "",
+  company: "",
+  email: "",
+  description: "",
 };
 
 const initialErrors = {
-  contactName: false,
-  contactEmail: false,
-  jobTitle: false,
-  companyName: false,
-  companyDescription: false,
-  companyAddress: false,
-  companyWebsite: false,
+  name: false,
+  company: false,
+  email: false,
+  description: false,
 };
 
 const PartnershipModal: React.FC<PartnershipModalProps> = ({
   onSubmit,
   onDismiss,
+  fromMetaverse = false,
 }) => {
-  const [values, setValues] = useState(initialValues);
+  const { data: userData, userDataLoaded } = useUser();
+  const [values, setValues] = useState(
+    userDataLoaded
+      ? {
+          ...initialValues,
+          name:
+            userData.firstName && userData.lastName
+              ? `${userData.firstName} ${userData.lastName}`
+              : "",
+        }
+      : initialValues
+  );
   const [errors, setErrors] = useState(initialErrors);
   const [pending, setPending] = useState(false);
-  const [checked, setChecked] = useState(false);
   const { toastSuccess, toastError } = useToast();
 
   const handleInputChange = (e) => {
@@ -100,13 +98,19 @@ const PartnershipModal: React.FC<PartnershipModalProps> = ({
 
   const validate = useCallback((): boolean => {
     let isValid = true;
-    const modifyErrors = initialErrors;
-    for (const [key, value] of Object.entries(values)) {
+    const modifyErrors = {
+      name: false,
+      company: false,
+      email: false,
+      description: false,
+    };
+    Object.entries(values).forEach((entry) => {
+      const [key, value] = entry;
       if (value === initialValues[key]) {
         isValid = false;
         modifyErrors[key] = true;
       }
-    }
+    });
     setErrors((prevState) => {
       return { ...prevState, ...modifyErrors };
     });
@@ -134,138 +138,67 @@ const PartnershipModal: React.FC<PartnershipModalProps> = ({
   );
 
   return (
-    <Modal title="Partnership Interest" onDismiss={onDismiss}>
+    <StyledModal title="Partnership" onDismiss={onDismiss}>
+      {fromMetaverse && (
+        <>
+          <Text mb="12px">
+            Please contact us to get access to the metaverse!
+          </Text>
+        </>
+      )}
       <form onSubmit={handleSubmit}>
-        <Label>Name (*)</Label>
+        <Label>Name</Label>
         <Input
           type="text"
-          placeholder="Contact Name"
-          name="contactName"
+          placeholder="Your Name"
+          name="name"
           onChange={handleInputChange}
         />
         <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.contactName ? "Contact name field cannot be empty" : ""
+          errors.name ? "Name field cannot be empty" : ""
         }`}</Text>
-        <Label>Email (*)</Label>
+        <Label color="black">Company</Label>
+        <Input
+          type="text"
+          placeholder="Company Name"
+          name="company"
+          onChange={handleInputChange}
+        />{" "}
+        <br />
+        <Text variant="bodySmall" color="failure" height="20px">{`${
+          errors.company ? "Company field cannot be empty" : ""
+        }`}</Text>
+        <Label color="black">Email</Label>
         <Input
           type="email"
-          placeholder="Contact Email"
-          name="contactEmail"
+          placeholder="Business Email Address"
+          name="email"
           onChange={handleInputChange}
-        />
+        />{" "}
+        <br />
         <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.contactEmail ? "Contact email field cannot be empty" : ""
+          errors.email ? "Email field cannot be empty" : ""
         }`}</Text>
-        <Label>Job Title (*)</Label>
-        <Input
-          placeholder="Contact Job Title"
-          name="jobTitle"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.jobTitle ? "Job title field cannot be empty" : ""
-        }`}</Text>
-        <Label>Company Name (*)</Label>
-        <Input
-          placeholder="Company Name"
-          name="companyName"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.companyName ? "Company name field cannot be empty" : ""
-        }`}</Text>
-        <Label>Company Description (*)</Label>
-        <Input
-          placeholder="Company Description"
-          name="companyDescription"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.companyDescription
-            ? "Company description field cannot be empty"
-            : ""
-        }`}</Text>
-        <Label>Company Address (*)</Label>
-        <Input
-          placeholder="Company Address"
-          name="companyAddress"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.companyAddress ? "Company address field cannot be empty" : ""
-        }`}</Text>
-        <Label>Company Website (*)</Label>
-        <Input
-          placeholder="Company Website"
-          name="companyWebsite"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px">{`${
-          errors.companyWebsite ? "Company website field cannot be empty" : ""
-        }`}</Text>
-        <Label>Company Twitter</Label>
-        <Input
-          placeholder="Company Twitter"
-          name="companyTwitter"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px" />
-        <Label>Company Instagram</Label>
-        <Input
-          placeholder="Company Instagram"
-          name="companyInstagram"
-          onChange={handleInputChange}
-        />
-        <Text variant="bodySmall" color="failure" height="20px" />
-        <Label>Additional Message</Label>
+        <Label color="black">Description</Label>
         <TextArea
-          placeholder="Additional Message"
-          name="additionalMessage"
+          placeholder="Describe your Business"
+          name="description"
           onChange={handleInputChange}
         />
-        <Flex my="16px">
-          <Checkbox onChange={(e) => setChecked(e.target.checked)} />
-          <Flex justifyContent="center" alignItems="center" ml="6px">
-            <InlineText variant="bodySmall">I agree with</InlineText>
-            <InlineLink
-              variant="bodySmall"
-              mx="6px"
-              href="/privacy"
-              target="_blank"
-            >
-              Privacy policy
-            </InlineLink>
-            <InlineText variant="bodySmall">|</InlineText>
-            <InlineLink
-              variant="bodySmall"
-              mx="6px"
-              href="/seller"
-              target="_blank"
-            >
-              Seller policy
-            </InlineLink>
-            <InlineText variant="bodySmall">|</InlineText>
-            <InlineLink
-              variant="bodySmall"
-              mx="6px"
-              href="/termsofuse"
-              target="_blank"
-            >
-              Terms of use
-            </InlineLink>
-          </Flex>
-        </Flex>
+        <Text variant="bodySmall" color="failure" height="20px">{`${
+          errors.description ? "Describe field cannot be empty" : ""
+        }`}</Text>
         {pending ? (
           <IconButton isLoading variant="text">
             <Spinner size={48} />
           </IconButton>
         ) : (
-          <Button variant="text" type="submit" p="0" disabled={!checked}>
+          <Button variant="text" type="submit" p="0">
             Submit
           </Button>
         )}
       </form>
-    </Modal>
+    </StyledModal>
   );
 };
 
