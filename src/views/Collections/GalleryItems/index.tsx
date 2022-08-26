0dcbeafebbdeeb1cb108a4f6390cdd512141658e
tony <ng.tony.dev@gmail.com>
2022-08-26 14:50:43 -0400

    Fixed large resolution gallery items

--- src/views/Collections/GalleryItems/index.tsx
+++ src/views/Collections/GalleryItems/index.tsx
@@ -8,7 +8,7 @@ interface GalleryProps {
 
 const GalleryItems: React.FC<GalleryProps> = ({ item }) => {
   return (
-    <Flex m="1rem" flexDirection="column">
+    <Flex m="1rem" flexDirection="column" maxWidth={310}>
       <a href={`/collection/${item.id}`}>
         <Image
           src={item.imageUrl}
@@ -18,9 +18,9 @@ const GalleryItems: React.FC<GalleryProps> = ({ item }) => {
           imageStyle={{ borderRadius: "20px" }}
           mb="1rem"
         />
+        <Text variant="h4Bold">{item.name}</Text>
+        <Text>{item.nftCount}</Text>
       </a>
-      <Text variant="h4Bold">{item.name}</Text>
-      <Text>{item.nftCount}</Text>
     </Flex>
   );
 };
